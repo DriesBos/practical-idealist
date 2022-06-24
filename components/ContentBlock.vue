b6d023d86a5fb601219fb196b65bb6fd3fa58947
DriesBos <info@driesbos.com>
2022-06-24 14:07:55 +0100

    ContentBlock component

---
+++ components/ContentBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section-ContentBlock">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass">
+.section-ContentBlock
+  background-color: #B4ABAB
+  width: auto
+  margin: 0 1em
+  padding: var(--spacing-sides) 0
+  border-radius: 60px
+</style>
---
+++ pages/curations/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page">
+    <ContentBlock>
+      <Intro
+        titleStart="Original"
+        titleEnd="Essays"
+        textContent="The PRACTICAL IDEALIST is dedicated to supporting and amplifying
+          innovation – both technological and intellectual – which, as our name
+          declares, combines the practical and the idealistic. We publish pieces
+          by a variety of thinkers on a variety topics, freely traversing styles
+          and perspectives. No matter the style, discipline, or subject matter,
+          we value systems thinking, actionable idealism, and beauty—because
+          without beauty, it’s all for nought."
+      />
+    </ContentBlock>
+  </div>
+</template>
+
+<script>
+import { getAllPosts } from "../../api/posts"
+import onPageLoad from "@/mixins/onPageLoad"
+
+export default {
+  mixins: [onPageLoad],
+  async asyncData() {
+    const posts = await getAllPosts()
+    return { posts: posts }
+  }
+}
+</script>
